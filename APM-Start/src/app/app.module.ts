e08d4455cf02861b5bccbecc7a17e6e3dab06bf0
bhanupriyac <bhanupriyac@eoxvantage.in>
2023-01-10 17:29:11 +0530

    Business Profile component -> card1 ,card2,card3 ,shield.png, app.component files for generic css styles

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { bpCard1Component } from './business-profile-card1-component/bp-card1.co
 import { bpCard2Component } from './business-profile-card2-component/bp-card2.component';
 import { bpCard3Component } from './business-profile-card3-component/bp-card3.component';
 import { businessProfile } from './business-profile/business-profile.component';
+import { noPaymentsRequired } from './no-payments-required-component/no-payments-required-component';
 import { ProductListComponent } from './products/product-list.component';
 import { ButtonComponent } from './shared/button-component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
@@ -23,6 +24,7 @@ import { StarComponent } from './shared/star.component';
     bpCard1Component,
     bpCard2Component,
     bpCard3Component,
+    noPaymentsRequired
   ],
   imports: [
     BrowserModule,
